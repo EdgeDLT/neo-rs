0e956fdcaa08dc10633f5b810d1dab3c05441f92
Jinghui Liao <jinghui@wayne.edu>
2021-03-02 00:12:28 -0500

    update

---
+++ SmartContract/src/lib.rs
@@ -0,0 +1,13 @@
+mod ContractParameterType;
+mod FindOptions;
+mod TriggerType;
+mod CallFlags;
+mod OpCode;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
